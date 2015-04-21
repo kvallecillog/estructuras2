ad7a0051f47001fa51a985017b9c0ec7838bf50f
kenneth <kvallecillog@gmail.com>
2015-04-20 18:56:06 -0600

    Maquina de estados adaptada a la tarea, compilada!

--- FSM.v
+++ FSM.v
@@ -1,28 +1,56 @@
 
 `timescale 1ns / 1ps
-`define ESTADO_A 0
-`define ESTADO_B 1
-`define ESTADO_C 2
+`define IDLE 0
+`define CALC 1
+`define DONE 2
+
+// FSM, maquina de estados finita
 
 module FSM
 (
+
+// Señales de la FSM
+
 	input wire Clock,
 	input wire Reset,
+	input wire valid_data,
+	input wire ack,
 	input wire In,
-	output reg Out
+	output reg Out,
+
+// Señales del datapath
+
+	input wire b_lsb,
+	output reg a_sel,
+	output reg b_sel,
+	output reg prod_sel,
+	output reg add_sel
+
 );
+
+
+	// Registros de estado actual y proximo estado.
+
 	reg [1:0] CurrentState, NextState;
+
+	// Registro proxima salida
+
 	reg NextOut; //Solo para hacer la salida sincrónica
 	
+
 	//Logica de Estado Presente y de Salida Sincrónica
+	
 	always @ ( posedge Clock, posedge Reset )
 
 	begin
+	
+	// Si ocurre reset, coloque la maquina en el estado IDLE.
+
 		if (Reset)
-		
+
 			begin
 		
-			CurrentState <= `ESTADO_A;
+			CurrentState <= `IDLE;
 			Out <= 0;	
 		
 			end
@@ -40,6 +68,7 @@ module FSM
 	
 	
 	//Lógica de Próximo Estado de Máquina Moore (Salida solo depende del estado)
+	
 	always @ ( * )
 
 	begin
@@ -47,38 +76,71 @@ module FSM
 		case (CurrentState)
 			
 
+			`IDLE:
+			begin
+				
+				NextOut = 0;
+				
+				// Si valid_data es 0 mantengase en el estado IDLE.
 
-			`ESTADO_A:
-			begin
-				NextOut = 0;
-				if (In == 0)
-					NextState = `ESTADO_B;
+				if (valid_data == 0)
+
+					NextState = `IDLE;
+				
+				// Si valid_data es 1 vaya al estado CALC.
+
 				else
-					NextState = `ESTADO_B;
+
+					NextState = `CALC;
 			end
-			`ESTADO_B:
+			
+
+			`CALC:
+			
 			begin
+
 				NextOut = 1;
+
 				if (In == 0)
-					NextState = `ESTADO_A;
+					NextState = `IDLE;
 				else
-					NextState = `ESTADO_C;
+					NextState = `DONE;
+
 			end
-			`ESTADO_C:
+			
+
+
+			`DONE:
+
+			// El estado DONE envia el mensaje hacia afuera del multiplicador, 
+			// el resultado se mantiene hasta que se reciba la señal ack que indica que el resultado fue leído.
+			
 			begin
+
 				NextOut = 0;
-				if (In == 0)
-					NextState = `ESTADO_B;
+
+				// Si ack es 0 mantengase en el estado DONE.
+				
+				if (ack == 0)
+				
+					NextState = `DONE;
+				
+				// Si ack es 1 vaya al estado IDLE.
 				else
-					NextState = `ESTADO_C;
+				
+					NextState = `IDLE;
+
 			end
+			
 			default:
+			
 			begin
-				NextState = `ESTADO_A;
+
+				NextState = `IDLE;
 				NextOut = 0;
+
 			end
 		
-
 
 		endcase
 
