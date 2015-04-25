b59f3ec089c603fb62a346ca696cad75cd937f5e
mora260 <mora1472@gmail.com>
2015-04-24 19:35:23 -0600

    Creo que esta listo

--- multiplicador.v
+++ multiplicador.v
@@ -2,10 +2,15 @@
 `include "datapath.v"
 `include "FSM.v"
 
-module multiplicador(prod, a, b, clk);
+module multiplicador(prod, Done_Flag, a, b, clk, reset, valid_data, ack);
 
-datapath data(a,b,prod,b_sel,a_sel,prod_sel,b_lsb,add_sel,clk,reset);
+	output [31:0] prod;
+	output Done_Flag;
+	input [15:0] a, b;
+	input clk,reset,valid_data,ack;
 
-FSM control(Clock, Reset, valid_data, ack, b_lsb, a_sel, b_sel, prod_sel, Done_Flag);
+	datapath data(a, b, prod, b_sel, a_sel, prod_sel, b_lsb, add_sel, clk, reset);
+
+	FSM control(clk, reset, valid_data, ack, b_lsb, a_sel, b_sel, prod_sel, Done_Flag);
 
 endmodule
--- testbench.v
+++ testbench.v
@@ -1,13 +1,11 @@
 `timescale 1ns/1ps
-`include "primitivas.v"
-`include "FSM.v"
 `include "multiplicador.v"
 
-module tester(clk, a,b, valid, prod);
+module tester(clk, reset, a, b, ack, valid, producto, ready);
 
 	output [15:0] a,b;
-	output clk;
-	input [31:0] prod;
+	output clk, ready;
+	input [31:0] producto;
 	input valid;
 
 	reg clk,valid;
@@ -30,7 +28,7 @@ module testbench;
 	wire [31:0] producto;
 	wire [15:0] a,b;
 	
-	tester test(clk,a,b,valid,producto);
-	FSM control();
+	tester test(clk, reset, a, b, ack, Done_Flag, producto, ready);
+	multiplicador imul(producto, Done_Flag, a, b, clk, reset, ready, ack);
 
 endmodule
