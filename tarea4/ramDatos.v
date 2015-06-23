ecad7110201147155b13addb49a7a8b2e4ad3a47
kenneth <kvallecillog@gmail.com>
2015-06-23 16:15:09 -0600

    Memoria ram de datos wires

--- tarea4/ramDatos.v
+++ tarea4/ramDatos.v
@@ -5,14 +5,14 @@
 // ADDR_WIDTH = 10 bits, bits para indexarla.
 // DATA_WIDTH = 8 bits por dato.
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
-module RAM_SINGLE_READ_PORT # ( parameter DATA_WIDTH= 8, parameter ADDR_WIDTH=10, parameter MEM_SIZE=1024 )
+module DATA_MEM # ( parameter DATA_WIDTH= 8, parameter ADDR_WIDTH=10, parameter MEM_SIZE=1024 )
 (
 input wire Clock,
 input wire iWriteEnable,
-input wire[ADDR_WIDTH-1:0] iReadAddress,
-input wire[ADDR_WIDTH-1:0] iWriteAddress,
-input wire[DATA_WIDTH-1:0] iDataIn,
-output reg [DATA_WIDTH-1:0] oDataOut
+input wire[ADDR_WIDTH-1:0] iReadDataAddress,
+input wire[ADDR_WIDTH-1:0] iWriteDataAddress,
+input wire[DATA_WIDTH-1:0] iDataMemIn,
+output reg [DATA_WIDTH-1:0] oDataMemOut
 );
 
 reg [DATA_WIDTH-1:0] Ram [MEM_SIZE:0];
